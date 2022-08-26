a6e748cd38dcfafa00136a46ce4112b8643c1805
Markus <mark394e@stud.kea.dk>
2022-08-26 13:11:14 +0200

    turning strings into numbers

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 
 document.addEventListener("DOMContentLoaded", start);
 
+let firstNumber = document.querySelector("#firstnumber");
+let secondNumber = document.querySelector("#secondnumber");
+let readOperator = document.querySelector("#operator");
+
 function start() {
+  firstNumber.value;
+  secondNumber.value;
+  readOperator.value;
+
+  let firstNumberAsNumber = parseInt(firstNumber.value);
+  let secondNumberAsNumber = parseInt(secondNumber.value);
+  //   console.log(readOperator.value);
   document.querySelector("#calculate").addEventListener("click", clickCalculate);
 }
 
