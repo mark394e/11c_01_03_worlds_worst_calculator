a2842b2aeb81b6e2ada79458342866c16fcd2010
Markus <mark394e@stud.kea.dk>
2022-08-26 12:35:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  document.querySelector("#calculate").addEventListener("click", clickCalculate);
+}
+
+function clickCalculate() {}
