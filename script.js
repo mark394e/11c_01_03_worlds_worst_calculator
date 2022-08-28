37699972118377a3dc639c8c66a21b1169ae0911
Markus <mark394e@stud.kea.dk>
2022-08-28 15:09:37 +0200

    "round to" functional

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// forces the script to be in strict mode which makes it easier to write "secure" JavaScript
 "use strict";
 
+// waiting for DOM to load
 document.addEventListener("DOMContentLoaded", start);
 
 const firstNumber = document.querySelector("#firstnumber");
@@ -8,7 +10,9 @@ const readOperator = document.querySelector("#operator");
 const doRound = document.querySelector("#doround");
 
 const resultList = document.querySelector("#results");
-const result = document.createElement("li");
+const resultAsTxt = document.createElement("li");
+
+let result;
 
 function start() {
   firstNumber.value;
@@ -21,38 +25,68 @@ function start() {
 }
 
 function clickCalculate() {
-  const result = document.createElement("li");
+  const resultAsTxt = document.createElement("li");
   let firstNumberAsNumber = parseFloat(firstNumber.value);
   let secondNumberAsNumber = parseFloat(secondNumber.value);
   if (readOperator.value == "add") {
-    firstNumber.value = firstNumberAsNumber + secondNumberAsNumber;
+    result = firstNumberAsNumber + secondNumberAsNumber;
   } else if (readOperator.value == "sub") {
-    firstNumber.value = firstNumberAsNumber - secondNumberAsNumber;
+    result = firstNumberAsNumber - secondNumberAsNumber;
   } else if (readOperator.value == "mul") {
-    firstNumber.value = firstNumberAsNumber * secondNumberAsNumber;
+    result = firstNumberAsNumber * secondNumberAsNumber;
   } else if (readOperator.value == "div") {
-    firstNumber.value = firstNumberAsNumber / secondNumberAsNumber;
+    result = firstNumberAsNumber / secondNumberAsNumber;
   }
 
   if (doRound.checked == true) {
-    roundNumber();
+    roundNumber(result);
+  } else {
+    firstNumber.value = result;
+    resultAsTxt.textContent = result;
+    resultList.appendChild(resultAsTxt);
+    document.querySelector("#results").scrollTo(0, 1000000);
+
+    secondNumber.value = "";
+    start();
+  }
+}
+
+function roundNumber(_result) {
+  let roundValue = document.querySelector("#decimals").value;
+  console.log(roundValue);
+
+  if (roundValue === "0") {
+    result = result.toFixed();
+  } else if (roundValue == "1") {
+    result = result.toFixed(1);
+  } else if (roundValue == "2") {
+    result = result.toFixed(2);
+  } else if (roundValue == "3") {
+    result = result.toFixed(3);
+  } else if (roundValue == "4") {
+    result = result.toFixed(4);
+  } else if (roundValue == "5") {
+    result = result.toFixed(5);
+  } else if (roundValue == "6") {
+    result = result.toFixed(6);
+  } else if (roundValue == "7") {
+    result = result.toFixed(7);
+  } else if (roundValue == "8") {
+    result = result.toFixed(8);
   }
 
-  result.textContent = firstNumber.value;
-  let resultAsNumber = parseFloat(result.textContent);
-  resultList.appendChild(result);
+  const resultAsTxt = document.createElement("li");
+  firstNumber.value = result;
+  resultAsTxt.textContent = result;
+  resultList.appendChild(resultAsTxt);
   document.querySelector("#results").scrollTo(0, 1000000);
 
   secondNumber.value = "";
   start();
 }
 
-function roundNumber() {
-  let roundValue = document.querySelector("#decimals").value;
-  console.log(roundValue);
-}
-
 function clearResults() {
+  // while the resultList still has a firstchild remove the child from resultList as long as it is the firstchild
   while (resultList.firstChild) {
     resultList.removeChild(resultList.firstChild);
   }
